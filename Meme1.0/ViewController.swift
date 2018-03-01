a224b92b609e49fb1a969217fb7a824de7193540
ginnigarg <guneetgarg.2011@gmal.com>
2018-03-01 17:28:26 +0530

    Initial Commit

---
+++ Meme1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme1.0
+//
+//  Created by Guneet Garg on 10/12/1939 Saka.
+//  Copyright © 1939 Saka Guneet Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
